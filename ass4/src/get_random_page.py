e7e67178a61c654673101a77a828dbeac20f1c2a
Itamar Shalev <itamar1111111@gmail.com>
2026-01-12 21:11:29 +0200

    ass4: Fix get_random_page to correctly resolve local HTML path
    The script now prepends the full absolute path to the HTML file
    instead of using the relative `pages/<html_file>` path, ensuring that
    the page can be opened correctly from the filesystem.
    
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- ass4/src/get_random_page.py
+++ ass4/src/get_random_page.py
@@ -1,11 +1,15 @@
 import random
 import json
 import webbrowser
+from pathlib import Path
 
 lines = open("page_urls.jsonl").readlines()
 selected = json.loads(random.choice(lines))
 print(selected["url"])
 print(selected["file_name"])
 print(selected["file_name"].split("/")[-1].split(".")[0])
-webbrowser.open(selected["file_name"])
 
+current_dir = Path(__file__).parent.absolute()
+url = current_dir / selected["file_name"]
+
+webbrowser.open(url)
